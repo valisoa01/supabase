167aa2ef03382d6dea1c4a4b4d80545374dc1e0c
valisoa01 <valisoatolotriniaina@gmail.com>
2026-09-14 16:51:58 +0300

    fix(key): supabase

--- lib/core/constants/supabase_config.dart
+++ lib/core/constants/supabase_config.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class SupabaseConfig {
-  static const String projectUrl = 'https://dtchhjesqwqkukdezotq.supabase.co';
-  static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0Y2hoamVzcXdxa3VrZGV6b3RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzODQzMDMsImV4cCI6MjEwNDk2MDMwM30.fXznZKdOPD9yIhFhpv_OgjRsauu6-8rb8eexF6wmH3E';
+  static String get projectUrl => dotenv.env['SUPABASE_URL'] ?? '';
+  static String get anonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'features/auth/presentation/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   runApp(const ProviderScope(child: MyApp()));
 }
 
